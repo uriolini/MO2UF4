4c5751074a0608d0ff46f9e91d5a48518a21babe
enti <enti@enti.enti>
2022-02-18 11:12:56 +0100

    Clase 18/02

--- index.js
+++ index.js
@@ -3,11 +3,53 @@
 
 let http = require("http");
 
+let mongo_client = require("mongodb").MongoClient;
+
+let url = "mongodb://localhost/";
+
+let db;
+
+console.log("Inicuando script mongo-http");
+
+mongo_client.connect(url, function(error, conn) {
+	console.log("Dentro de MongoDB");
+
+	if(error){
+		console.log("ERROR!!!");
+		return;
+	}
+
+	db = conn.db("tffhd");
+});
+
+
+
+
 http.createServer(function(request, response){
 	response.writeHead(200);
 
-	let saludo = "<h1>ola k ase</h1>";
+	if(request.url == "/"){
+		response.end();
+		return;
+	}
 
-	response.end(saludo);
+	let col= "";
+	
+	if(request.url == "/characters")
+		col = "characters";
+	else if(request.url == "/items")
+		col = "items";
+	else if(request.url == "/weapons")
+		col = "weapons";
+	else
+		return;
+
+	let col_data = db.collection(col).find();
+
+	col_data.toArray(function(err,data){
+		let string = JSON.stringify(data);
+
+		response.end(string);
+	});
 
 }).listen(1095);
